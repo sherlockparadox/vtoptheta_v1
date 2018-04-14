e0a6b6239a1a0cfb4821e86e09fe190c2f67da11
Kapil Shrotriya <cross399@gmail.com>
2018-04-14 23:31:55 +0530

    Fixed noob warning

--- theta_v1/src/components/autocomplete_search.js
+++ theta_v1/src/components/autocomplete_search.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 import Input from 'material-ui/Input';
-import TextField from 'material-ui/TextField';
 import { MenuItem } from 'material-ui/Menu';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import CancelIcon from '@material-ui/icons/Cancel';
