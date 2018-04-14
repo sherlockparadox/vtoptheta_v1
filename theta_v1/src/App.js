05e86e8c906e07f6642131023c720fca9503a823
sherlockparadox <architsingh007@gmail.com>
2018-04-14 22:33:18 +0530

    Site div struct
    Div structure fo the complete site.

--- theta_v1/src/App.js
+++ theta_v1/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Wrapper01">
+        
+        <div className="Premium">
+            <div className="search_size">
+            </div>
+        </div>
+        
+        <div className="menu">
+        </div>
+        
+        <div className="right-box">
+        </div>
       </div>
     );
   }
